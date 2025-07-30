7212fd670e7152178deffeb70089ff3863a51348
NathanielAGR <nathaniel.yeo@agr.gc.ca>
2025-07-30 14:50:44 -0600

    Updated pig sorting api to be able to send mongoDB json objects.

--- src/pig-sorting-api/client.py
+++ src/pig-sorting-api/client.py
@@ -1,11 +1,11 @@
 from pymongo import MongoClient
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 import os
 
 
 
 
-load_dotenv()
+#load_dotenv()
 
 #get credentials from .env
 username = os.getenv("MONGO_INITDB_ROOT_USERNAME")
@@ -20,11 +20,17 @@ uri = f"mongodb://{username}:{password}@{host}:{port}/"
 #connect to the server
 client = MongoClient(uri)
 
-#connect to/create the database
-db = client["meat_evaluation"]
+# #connect to/create the database
+# db = client["meat_evaluation"]
 
-#connect to/create the table
-db_result = db["result"]
+# #connect to/create the table
+# db_result = db["result"]
+
+# Connect to database
+db = client["visionPipe"]
+
+# Connect to the table
+db_result = db["metaData"]
 
 
 #def saveResult(result):
--- src/pig-sorting-api/main.py
+++ src/pig-sorting-api/main.py
@@ -11,10 +11,14 @@ from io import BytesIO
 from datetime import datetime
 import numpy as np
 import cv2
-from client import db_result# this contains our connection to mongoDB
+import pandas as pd
+from client import db# this contains our connection to mongoDB
 from pymongo.results import InsertOneResult
 import os
 from bson import ObjectId
+import bson
+import logging
+import json
 
 
 app = FastAPI()
@@ -27,6 +31,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+logger = logging.getLogger()
 
 
 def image_to_base64(img: np.ndarray) -> str:
@@ -40,76 +45,96 @@ def image_to_base64(img: np.ndarray) -> str:
     return base64.b64encode(buffer).decode()
 
 
-
-@app.post("/api/v1/predict/", response_class=JSONResponse)
-async def predict(file: UploadFile = File(...), model_id: int = Query(2), colour_corrected: bool = True, ground_truth:int=None):
+@app.get("/api/v1/mongoData", response_class=JSONResponse)
+async def get_mongo_data():
     try:
-        image_bytes = await file.read()
-        image = Image.open(io.BytesIO(image_bytes)).convert("RGB") # PIL Image
-        
-        #perform colour correction if Colour correct is true
-        if colour_corrected:
-           #converting it to a numpy array becuase that is what colour corrector expects
-           image_bgr = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
-           image = cc.correct(image_bgr)
-           image = (image * 255).astype(np.uint8)  # Ensure it's uint8 for YOLO
-        result = run_model(image, model_id)
-        print("before submit result function")
-        
-        #adding tag for if it was colour corrected
-        result["colour_corrected"]=colour_corrected
-        
-        #add tag for if it were classification or segmentation
-        result["type"]="classification" if model_id==1 else "segmentation"
-        
-        #add groundtruth if available
-        result["ground_truth"]=ground_truth
-        
-        submitResult(result, image, file.filename)
-        
-        #print(f"result after submit result: {result}")
-        return result
-    except Exception as e:
-        # Log the exception for debugging purposes
-        print(f"Error in predict endpoint: {e}")
-        return JSONResponse(content={"error": str(e)}, status_code=500)
+        db_result = db["metaData"]
+        result = db_result.find()
 
-class ImageData(BaseModel):
-    image: str
-    model_id: int = 2
+        #Parse JSON string into python objects to allow creation of a dataframe
+        meta_data_json = json.loads(bson.json_util.dumps(result))
 
-@app.post("/api/v1/predict_base64/", response_class=JSONResponse)
-async def predict_base64(data: ImageData, colour_corrected: bool = True, ground_truth:int=None):
-    try:
-        image_bytes = base64.b64decode(data.image)
-        image = Image.open(BytesIO(image_bytes)).convert("RGB") # PIL Image
         
-        #perform colour correction if Colour correct is true
-        if colour_corrected:
-            #converting it to a numpy array becuase that is what colour corrector expects
-           image_bgr = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
-           image = cc.correct(image_bgr)
-           image = (image * 255).astype(np.uint8)  # Ensure it's uint8 for YOLO
-        
-        result = run_model(image, data.model_id)
-        
-        #adding tag for if it was colour corrected
-        result["colour_corrected"]=colour_corrected
-        
-        
-        #add tag for if it were classification or segmentation
-        result["type"] = "classification" if data.model_id == 1 else "segmentation"
-        
-        #add groundtruth if available
-        result["ground_truth"]=ground_truth
-        
-        submitResult(result, image)
-        #print(f"result after submit result{result}")
-        return result
+
+        return meta_data_json
+            
+
     except Exception as e:
-        # Log the exception for debugging purposes
-        print(f"Error in predict_base64 endpoint: {e}")
-        return JSONResponse(content={"error": str(e)}, status_code=500)
+        logger.info(f"Error getting data :{e}")
+        return JSONResponse(status_code=500, content={"error": str(e)})
+
+
+
+
+# @app.post("/api/v1/predict/", response_class=JSONResponse)
+# async def predict(file: UploadFile = File(...), model_id: int = Query(2), colour_corrected: bool = True, ground_truth:int=None):
+#     try:
+#         image_bytes = await file.read()
+#         image = Image.open(io.BytesIO(image_bytes)).convert("RGB") # PIL Image
+        
+#         #perform colour correction if Colour correct is true
+#         if colour_corrected:
+#            #converting it to a numpy array becuase that is what colour corrector expects
+#            image_bgr = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+#            image = cc.correct(image_bgr)
+#            image = (image * 255).astype(np.uint8)  # Ensure it's uint8 for YOLO
+#         result = run_model(image, model_id)
+#         print("before submit result function")
+        
+#         #adding tag for if it was colour corrected
+#         result["colour_corrected"]=colour_corrected
+        
+#         #add tag for if it were classification or segmentation
+#         result["type"]="classification" if model_id==1 else "segmentation"
+        
+#         #add groundtruth if available
+#         result["ground_truth"]=ground_truth
+        
+#         submitResult(result, image, file.filename)
+        
+#         #print(f"result after submit result: {result}")
+#         return result
+#     except Exception as e:
+#         # Log the exception for debugging purposes
+#         print(f"Error in predict endpoint: {e}")
+#         return JSONResponse(content={"error": str(e)}, status_code=500)
+
+# class ImageData(BaseModel):
+#     image: str
+#     model_id: int = 2
+
+# @app.post("/api/v1/predict_base64/", response_class=JSONResponse)
+# async def predict_base64(data: ImageData, colour_corrected: bool = True, ground_truth:int=None):
+#     try:
+#         image_bytes = base64.b64decode(data.image)
+#         image = Image.open(BytesIO(image_bytes)).convert("RGB") # PIL Image
+        
+#         #perform colour correction if Colour correct is true
+#         if colour_corrected:
+#             #converting it to a numpy array becuase that is what colour corrector expects
+#            image_bgr = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+#            image = cc.correct(image_bgr)
+#            image = (image * 255).astype(np.uint8)  # Ensure it's uint8 for YOLO
+        
+#         result = run_model(image, data.model_id)
+        
+#         #adding tag for if it was colour corrected
+#         result["colour_corrected"]=colour_corrected
+        
+        
+#         #add tag for if it were classification or segmentation
+#         result["type"] = "classification" if data.model_id == 1 else "segmentation"
+        
+#         #add groundtruth if available
+#         result["ground_truth"]=ground_truth
+        
+#         submitResult(result, image)
+#         #print(f"result after submit result{result}")
+#         return result
+#     except Exception as e:
+#         # Log the exception for debugging purposes
+#         print(f"Error in predict_base64 endpoint: {e}")
+#         return JSONResponse(content={"error": str(e)}, status_code=500)
         
         
 # Helper function to convert ObjectId to string
@@ -149,6 +174,7 @@ def submitResult(result, image, filename=None):
     
     # Save the result into MongoDB
     try:
+        db_result = db["result"]
         insert_result = db_result.insert_one(db_result_data)
         if isinstance(insert_result, InsertOneResult) and insert_result.acknowledged:
             print(f"[INFO] Successfully inserted result into MongoDB: {insert_result.inserted_id}")
